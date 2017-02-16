64300cec9e8b26bb6458f9a754bd7b2560f3deb6
shun3475 <imokenpi3475@gmail.com>
2017-02-16 13:57:37 +0900

    Use Azure Computer Vision API

--- mqtt_server.js
+++ mqtt_server.js
@@ -1,8 +1,8 @@
 const mqtt = require('mqtt')
 const getImagePath = require('./lib/get_image_path')
-const translate = require('./lib/translate')
+const getTranslateText = require('./lib/translate')
 const googleVision = require('node-cloud-vision-api')
-const conf = require('../config.json')
+const conf = require('./config.json')
 const analyseImage = require('./lib/analyse_image')
 
 const client  = mqtt.connect(
@@ -21,14 +21,15 @@ client.on('message', function (topic, message) {
 	console.log('\n')
 	console.log('----- Receiving message -----')
 	console.log(topic)
-	if (topic == './image') {
+	if (topic == 'sh8@github/jphacks/image') {
     getImagePath(
       message.toString(),
       (filePath) => {
         // Using Azure Computer Vision API
-				analyseImage(filePath, (text) => {
-					console.log(text)
-					translate(text, 'ja', (translation) => {
+				analyseImage(filePath, (items) => {
+					//TODO Should check all items?
+					var text = items[0]
+					getTranslateText(text, (translation) => {
 						console.log(translation)
 						client.publish('sh8@github/jphacks/result', translation)
 					})
